d664fd1f7b5894d98fb979ee754d49e8c1f009ce
ferseba18 <fer_seba_18@yahoo.com.ar>
2020-11-10 15:33:28 -0300

    Update CursoTest.java

--- test/ar/edu/unlam/escuela/CursoTest.java
+++ test/ar/edu/unlam/escuela/CursoTest.java
@@ -1,11 +1,9 @@
-package ar.edu.unlam.escuela;
-
-import static org.junit.Assert.*;
-
 import java.util.HashSet;
 
 import org.junit.Test;
 
+import ar.edu.unlam.escuela.Curso;
+import ar.edu.unlam.escuela.Materia;
 import ar.edu.unlam.personas.Alumno;
 import ar.edu.unlam.personas.Profesor;
 import junit.framework.Assert;
@@ -15,89 +13,133 @@ public class CursoTest {
 	@Test
 	public void testQueDevuelveProfesor() {
 		Curso c=new Curso("4C");
-		Materia m=new Materia(13, "Matematica");
-		Profesor p=new Profesor(12376521, "leandro");
-		Profesor p1=new Profesor(12314258, "Marcela");
-		Profesor p2=new Profesor(17812854, "Javier");
 		
-		Object VE = p;
+		Profesor p=new Profesor(12376521, "leandro");		
+		Materia m=new Materia(13, "Matematica",p);
+		
+		c.agregarMateria(m);
+		
+		Object VE =p ;
 		Object VO= c.bucarProfeEnCursoXDni(12376521);
 		assertEquals(VE, VO);
-}
+	}
+	
+	@Test
 	public void testQuebuscarProfeEnCurso() {
 		Curso c=new Curso("4C");
-		Materia m=new Materia(13, "Matematica");
-		Profesor p=new Profesor(12376521, "leandro");
-		Profesor p1=new Profesor(12314258, "Marcela");
+	
 		Profesor p2=new Profesor(17812854, "Javier");
-		Object VE= p;
-		Object VO=c.buscarProfeEnCurso(p);
+		Materia m=new Materia(13, "Matematica",p2);
+		
+		c.agregarMateria(m);
+		
+		Object VE= p2;
+		Object VO=c.buscarProfeEnCurso(p2);
+		
 		assertEquals(VE, VO);
 	}
+	
+	@Test
 	public void testQueBuscaMateriaPorID() {
 		Curso c=new Curso("4C");
-		Materia m=new Materia(13, "Matematica");
-		Materia m1=new Materia(12, "Geografia");
-		Materia m2=new Materia(11, "Historia");
+		
 		Profesor p=new Profesor(12314258, "Marcela");
-        Materia VE=null;
-        Materia VO=c.buscarMateriaXId(m.getIdMateria());
-        assertEquals(VE,VO);
-	}
-	public void testQueBuscaMateria() {
-		Curso c=new Curso("4C");
-		Materia m=new Materia(13, "Matematica");
-		Materia m1=new Materia(12, "Geografia");
-		Materia m2=new Materia(11, "Historia");
-		Profesor p=new Profesor(12314258, "Marcela");
-		Materia VE=null;
-        Materia VO=c.buscarMateria(m); 
+		
+		Materia m=new Materia(13, "Matematica",p);
+		Materia m1=new Materia(12, "Geografia",p);
+		Materia m2=new Materia(11, "Historia",p);
+		
+		c.agregarMateria(m);
+		c.agregarMateria(m1);
+		c.agregarMateria(m2);
+
+        Materia VE=m;
+        Materia VO=c.buscarMateriaXId(13);
         assertEquals(VE,VO);
 	}
 	
+	@Test
+	public void testQueBuscaMateria() {
+		Curso c=new Curso("4C");
+		
+		Materia m=new Materia(13, "Matematica");
+		Materia m1=new Materia(12, "Geografia");
+
+		
+		c.agregarMateria(m);
+		c.agregarMateria(m1);
+
+		
+		
+		Materia VE=m1;
+        Materia VO=c.buscarMateria(m1); 
+        assertEquals(VE,VO);
+	}
+	
+	@Test
 	public void testQueBorraMateria() {
 		Curso c=new Curso("4C");
 		Materia m=new Materia(13, "Matematica");
-		Materia m1=new Materia(12, "Geografia");
-		Materia m2=new Materia(11, "Historia");
-		Profesor p=new Profesor(12314258, "Marcela");
+
+		c.agregarMateria(m);
+		
 		Boolean VE=true;
-        Boolean VO=c.eliminarMateria(m1); 
+        Boolean VO=c.eliminarMateria(m); 
         assertEquals(VE,VO);
 	}
+	
+	@Test
 	public void testQueAgregaMateria() {
 		Curso c=new Curso("4C");
 		Materia m=new Materia(13, "Matematica");
 		Materia m1=new Materia(12, "Geografia");
 		Materia m2=new Materia(11, "Historia");
 		Profesor p=new Profesor(12314258, "Marcela");
-		Boolean VE=true;		
-		Materia m3=new Materia(22,"Psicologia");
+	    Boolean VE=true;		
+	    Materia m3=new Materia(22,"Psicologia");
         Boolean VO=c.agregarMateria(m3); 
         assertEquals(VE,VO);
 	}
+	
+	@Test
 	public void testQueBuscaAlumnoPorDNI() {
 		Curso c=new Curso("4C");
-		Materia m=new Materia(12, "Geografia");
-		Profesor p=new Profesor(12314258, "Marcela");
+		
 		Alumno a=new Alumno(32432456, "Sofia");
 		Alumno a1=new Alumno(32472345, "Marcelo");
 		Alumno a2=new Alumno(31794379, "Juan");
+		
+		c.agregarAlumno(a);
+		c.agregarAlumno(a2);
+		c.agregarAlumno(a1);
+		
 		Object VE=a;
 		Object VO=c.buscarAlumnoXDni(a.getDni());
 		assertEquals(VE,VO);
 	}
+	
+	@Test
 	public void testQueBorraAlumno() {
 		Curso c=new Curso("4C");
-		Materia m=new Materia(12, "Geografia");
+		
 		Profesor p=new Profesor(12314258, "Marcela");
+		Materia m=new Materia(12, "Geografia",p);
+		
 		Alumno a=new Alumno(32432456, "Sofia");
 		Alumno a1=new Alumno(32472345, "Marcelo");
 		Alumno a2=new Alumno(31794379, "Juan");
+		
+		c.agregarAlumno(a);
+		c.agregarAlumno(a2);
+		c.agregarAlumno(a1);
+		
+		
 		Boolean VE=true;
         Boolean VO=c.eliminarAlumno(a1); 
         assertEquals(VE,VO);
 	}
+	
+	@Test
 	public void testQueAgregeAlumno() {
 		Curso c=new Curso("4C");
 		Materia m=new Materia(12, "Geografia");
