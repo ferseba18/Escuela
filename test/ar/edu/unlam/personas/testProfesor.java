0ec9430791325b8afd217ef2ac01eee3ea8d2960
alejaxd <alejandroleach@hotmail.com>
2020-11-10 00:21:24 -0300

    Agrege testProfesor

---
+++ test/ar/edu/unlam/personas/testProfesor.java
@@ -0,0 +1,77 @@
+package ar.edu.unlam.personas;
+
+import ar.edu.unlam.escuela.Materia;
+import ar.edu.unlam.escuela.Notas;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testProfesor {
+
+	@Test
+	public void queSePuedaCrearProfesor() {
+		Profesor profesor = new Profesor(98765432, "Skiner");
+		
+		String nombre = "Skiner";
+		Integer dni = 98765432;
+		
+		assertEquals(nombre, profesor.getNombre());
+		assertEquals(dni, profesor.getDni());
+	}
+	
+	@Test
+	public void queSePuedaCargarNotaAUnAlumno() {
+		Profesor profesor = new Profesor(98765432, "Skiner");
+		Alumno alumno = new Alumno(40950675, "Alejandro");
+		Materia historia = new Materia(3, "Historia");
+		Notas nota1erTrimestre = new Notas(historia, 8);
+		
+		Boolean valorObtenido = profesor.cargarNotaAAlumno(alumno, 1, nota1erTrimestre);
+		
+		assertTrue(valorObtenido);
+	}
+	
+	@Test
+	public void queSePuedaCargarNotaFinalAUnAlumno() {
+		Profesor profesor = new Profesor(98765432, "Skiner");
+		Alumno alumno = new Alumno(40950675, "Alejandro");
+		Materia historia = new Materia(3, "Historia");
+		Notas notaFinal = new Notas(historia, 8);
+		
+		Boolean valorObtenido = profesor.cargarNotaAAlumno(alumno, 1, notaFinal);
+		
+		assertTrue(valorObtenido);
+	}
+	
+	@Test
+	public void queSePuedaEliminarNotaXTrimestre() {
+		Profesor profesor = new Profesor(98765432, "Skiner");
+		Alumno alumno = new Alumno(40950675, "Alejandro");
+		Materia historia = new Materia(3, "Historia");
+		Notas notaFinal = new Notas(historia, 8);
+		
+		profesor.cargarNotaAAlumno(alumno, 1, notaFinal);
+		Boolean valorObtenido = profesor.eliminarNotaPorTrimestre(alumno, 1, notaFinal);
+		
+		assertTrue(valorObtenido);
+	}
+	
+	@Test
+	public void queSePuedaEliminarNotaFinal() {
+		Profesor profesor = new Profesor(98765432, "Skiner");
+		Alumno alumno = new Alumno(40950675, "Alejandro");
+		Materia historia = new Materia(3, "Historia");
+		Notas notaFinal = new Notas(historia, 8);
+		
+		profesor.cargarNotaFinalAAlumno(alumno,notaFinal);
+		
+		Boolean valorObtenido = profesor.eliminarNotaFinal(alumno, notaFinal);
+		
+		assertTrue(valorObtenido);
+	}
+}
